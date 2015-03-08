e94d56d9f573d39ff3700b67c0d015e0f21c864a
arbarnett <barnetal@onid.oregonstate.edu>
2015-03-08 15:56:45 -0600

    delete an item

---
+++ deleteItem.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+$responseToAjax = array(
+	"error" => null,
+	"success" => true
+);
+
+//check if session has not been started / not a valid user
+if(session_status() != PHP_SESSION_ACTIVE || !isset($_SESSION['username']) ) {
+	$responseToAjax["error"] = "Invalid user. Please log in.";
+	$responseToAjax["success"] = false;
+} 
+
+//check if an item has been selected to be deleted
+else if (!isset($_GET["deleteid"]) || empty($_GET["deleteid"])) {
+	$responseToAjax["error"] = "Please select an item to delete.";
+	$responseToAjax["success"] = false;
+}
+
+else {
+	$id = $_GET["deleteid"];
+
+	$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "barnetal-db", "jVIV8TuG4g2sc4ER", "barnetal-db");
+	if ($mysqli->connect_errno) {
+		$responseToAjax["error"] = "There was an issue connecting to the database:". $mysqli->connect_errno . ")" . $mysqli->connect_error;
+		$responseToAjax["success"] = false;
+	} 
+	else {
+
+	$deleteItem = $mysqli->prepare("DELETE FROM Groceries WHERE id= ?"); 
+	$deleteItem->bind_param("i", $id);
+	$deleteItem->execute();
+	$deleteItem->close();
+
+	}
+}
+
+//respond to request
+echo json_encode($responseToAjax);
